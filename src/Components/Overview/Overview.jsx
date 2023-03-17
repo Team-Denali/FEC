72745f38d89ffa81aef12446e66693055a22b7a5
Chordata88 <jamescarringtoniii@gmail.com>
2023-03-17 19:20:47 -0400

    copied prior work to new fork

--- src/Components/Overview/Overview.jsx
+++ src/Components/Overview/Overview.jsx
@@ -1,11 +1,46 @@
-import React from 'react';// Bring React in to build a component.
+import React from 'react'; const axios = require('axios');// Bring React in to build a component.
 
-//import { createRoot } from "react-dom/client";
+import {useState, useEffect} from 'react'; import OverviewMainPic from './OverviewPicture/OverviewMainPic.jsx';
 
-
+import OverviewStyles from './OverviewInfo/OverviewStyles.jsx';
 // Huzzah for jsx!
-var Overview = () => { //include state variables for currently viewed product
-  return <h1>Overview</h1>
+var Overview = ({current}) => { //include state variables for currently viewed product
+const [itemView, setItemView] = useState(current); const [style, setStyle] = useState({}); let headers =  { Authorization: '*****' }; const [styleIndex, setIndex] = useState(0); const [photoIndex, setPhotoIndex] = useState(0); //${current.id}
+useEffect(() => {
+  axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/37313/styles`, {headers: headers} ).then((res) => { console.log(res.data); setStyle(res.data)} )
+
+
+}, [itemView])
+
+
+//className="productOverviewPic"
+
+  return (
+<>
+  <div className="overviewContainer">
+    <OverviewMainPic/>
+
+
+
+
+
+
+
+    <div className="overviewProductInfo">
+
+      <div>Review Stuff Goes Here</div>
+
+      <div>Category Filler</div>
+      <h1>Item Name Filler</h1>
+      <div><OverviewStyles/></div>
+
+    </div>
+
+
+  </div>
+
+
+  </>)
 }
 
 
---
+++ src/Components/Overview/OverviewPicture/OverviewMainPic.jsx
@@ -0,0 +1,19 @@
+import React from 'react';// Bring React in to build a component.
+
+var arr = [1, 2, 3];//import { createRoot } from "react-dom/client";
+
+// Huzzah for jsx!
+
+var OverviewMainPic = () => { //include state variables for currently viewed product
+  return (
+
+    <div className='productOverviewPic' style={{backgroundImage: `url(${'https://images.unsplash.com/photo-1492447105260-2e947425b5cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80'})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}>
+
+    {arr.map((num) => <div>{num}</div>)}
+
+    </div>
+    )
+}
+
+
+export default OverviewMainPic
