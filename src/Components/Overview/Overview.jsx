5ac0832b883cf2a03ebf5944c60b53b17eb7f906
Chordata88 <jamescarringtoniii@gmail.com>
2023-03-22 12:06:39 -0400

    incomplete troubleshooting for styles state implementation

--- src/Components/Overview/Overview.jsx
+++ src/Components/Overview/Overview.jsx
@@ -5,62 +5,53 @@ import {useState, useEffect} from 'react'; import OverviewMainPic from './Overvi
 import OverviewStyles from './OverviewInfo/OverviewStyles.jsx'; import OverviewCart from './OverviewInfo/OverviewCart.jsx'; // Huzzah for jsx!
 
 var Overview = ({current}) => { console.log(current, 'current prop')//include state variables for currently viewed product
-const [itemView, setItemView] = useState({}); const [styles, setStyles] = useState({}); const [loading, setLoading] = useState(true); let url = 'http://localhost:3000'; //console.log('the ID:', current.id) //const [styleIndex, setIndex] = useState(0); const [photoIndex, setPhotoIndex] = useState(0);
-// useEffect(() => { let id = current.id; console.log('THE ID:', id); let paramObj = { product_id: current.id };
-//   return axios.get(`${url}/styles`, { params: paramObj } ).then((res) => { console.log(res.data); setStyles(res.data); console.log('newStylesStatesss:', styles)} ).catch((err) => {console.log('axios req error:', err)})
+const [itemView, setItemView] = useState({}); const [styles, setStyles] = useState({}); const [styleView, setStyleView] = useState({}); let url = 'http://localhost:3000'; //const [styleIndex, setIndex] = useState(0); const [photoIndex, setPhotoIndex] = useState(0);
+const [mainPic, setMainPic] = useState(null)
+var changePic = (pic_url) => {setMainPic(pic_url)}
 
+var changeStyle = (style) => {setStyleView(style)};
 
-// }, [])
 const styleFinder = (currentId) => { let styleId = currentId; console.log('THE ID:', styleId); let paramObj = { product_id: styleId };
-  axios.get(`${url}/styles`, { params: paramObj } ).then((res) => { console.log( 'the data:', res.data); setStyles(res.data.results); } ).catch((err) => {console.log('axios req error:', err)})
+  axios.get(`${url}/styles`, { params: paramObj } ).then((res) => { console.log( 'the data:', res.data); setStyles(res.data.results); console.log('STYLES 0:', styles); } ).catch((err) => {console.log('axios req error:', err)})
 
 
 }
 useEffect(() => {styleFinder(current.id); }, [current])
-
-console.log('THE STYLES:', styles);//className="productOverviewPic"
-// if (loading) {
-
-
-
-// return (<div>STILL LOADING...</div>) }
+//changeStyle(styles[0]);
+console.log('THE STYLES:', styles);//className="productOverviewPic
+// setMainPic(styles[0]);
+//console.log('current itemview:', itemView)
 
 
 return (// }
-
 <> <div className="overviewContainer">
 
   <div className="overviewTop">
 
-    <OverviewMainPic currentStyle={styles}/>
-
-
-
-
-
-
+    <OverviewMainPic current={current} currentStyles={styles} style={styleView} setStyle={changeStyle} mainPic={mainPic} setMain={setMainPic} picHandler={changePic}/>
 
     <div className="overviewProductInfo">
       <div>Review Stuff Goes Here</div>
-
       <div>{current.category}</div>
-      <div className='productName'><h1>{current.name}</h1></div>
 
-      <div className='overviewStyles'><OverviewStyles overviewStyles={styles}/></div>
+      <div className='productName'><h1>{current.name}</h1></div>
+  <div><h3>{current.default_price}</h3></div>
+
+      <div className='overviewStyles'><OverviewStyles overviewStyles={styles} mainPic={mainPic} picHandler={changePic} item={itemView} setItem={setItemView}/></div>
       <div className="overviewCart"><OverviewCart/></div>
 
     </div>
   </div>
     <div className='overviewBottom'>
-
       <div>{current.description}</div>
       <div>||</div>
+
       <div>Insert Specs</div>
     </div>
 
   </div>
-
   </>)
-}
 
+
+}
 export default Overview
--- src/Components/Overview/OverviewInfo/OverviewStyles.jsx
+++ src/Components/Overview/OverviewInfo/OverviewStyles.jsx
@@ -1,30 +1,63 @@
-import React from 'react';// Bring React in to build a component.
+import React from "react"; // Bring React in to build a component.
 
-import {useState, useEffect} from 'react';//import { createRoot } from "react-dom/client";
+import { useState, useEffect } from "react"; //import { createRoot } from "react-dom/client";
 
 // Huzzah for jsx!
 
-var OverviewStyles = ({overviewStyles}) => { console.log('overviewstyles:', overviewStyles); //let stylesArr = overviewStyles.results; console.log(stylesArr, 'FSDKL:FKDSFJ');
+var OverviewStyles = ({
+  overviewStyles,
+  mainPic,
+  picHandler,
+  item,
+  setItem,
+}) => {
+  console.log("overviewstyles:", overviewStyles);
+  let style1 = overviewStyles[0];
+  console.log(style1, "STYLE1");
 
+  const [currentStyle, setCurrentStyle] = useState(style1); //setCurrentStyle(style1)
+  if (!overviewStyles.length || !Object.keys(overviewStyles).length) {
+    return;
+  }
+  // setCurrentStyle(overviewStyles[0]);
+  // useEffect(() => {
+  //   console.log("the currentstyle state:", currentStyle);
 
-if (!overviewStyles.length || (!(Object.keys(overviewStyles).length))) {
+  // }, [currentStyle]); //setDisplay(overviewStyles[i])
+  return (
 
-  return;
+    <>
+
+      <div className="overviewStylesContainer">
+
+        <div>STYLE → {overviewStyles[0].name}</div>
+        <div className="styleSelectorArea">
+          {overviewStyles.map((overviewStyle, i) => (
+
+            <div
+              key={i.toString()}
+              value={"thing"}
+              style={{
+                height: "35px",
+                width: "20px",
+                backgroundImage: `url(${overviewStyle.photos[0].thumbnail_url})`,
+                backgroundSize: "cover",
+                borderRadius: "50%",
+              }}
+              onClick={(e) => {
+                e.preventDefault();
+              }}
+            ></div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default OverviewStyles;
+
+{
+  /* <div>{styles.results.map((oneStyle) => (<div><img src="https://images.unsplash.com/photo-1552902865-b72c031ac5ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
+/></div></div>))} */
 }
-
-return (<>
-<div className='overviewStylesContainer'>
-
-  <div>STYLE → {overviewStyles[0].name}</div>
-  <div className='styleSelectorArea'>
-  {overviewStyles.map((overviewStyle, i) => <div key={i} style={{height: '35px', width: '20px', backgroundImage: `url(${overviewStyle.photos[0].thumbnail_url})`, backgroundSize: 'cover'}}></div>)}</div>
-</div>
-
-</>)
-
-}
-
-export default OverviewStyles
-
-{/* <div>{styles.results.map((oneStyle) => (<div><img src="https://images.unsplash.com/photo-1552902865-b72c031ac5ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
-/></div></div>))} */}
