d4fc1b2c6db20d8cc3607160543a60e685e88233
Rev Tan <tanyunzhe98@gmail.com>
2023-03-28 21:43:26 -0400

    use lazy to import component

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,14 @@
 import React from 'react';// Bring React in to build a component.
-import { useState, useEffect } from 'react';
-import { createRoot } from "react-dom/client"; import Overview from './Components/Overview/Overview.jsx'; import QuestionsAnswers from './Components/QnA/QnA.jsx'; import RelatedItems from './Components/RelatedItems/RelatedItems.jsx'; import Reviews from './Components/Reviews/Reviews.jsx';
+import { lazy, useState, useEffect } from 'react';
+import { createRoot } from "react-dom/client";
+const Overview = lazy(() => import("./Components/Overview/Overview.jsx"));
+const QuestionsAnswers = lazy(() => import("./Components/QnA/QnA.jsx"));
+const RelatedItems = lazy(() => import("./Components/RelatedItems/RelatedItems.jsx"));
+const Reviews = lazy(() => import("./Components/Reviews/Reviews.jsx"));
+// import Overview from './Components/Overview/Overview.jsx';
+// import QuestionsAnswers from './Components/QnA/QnA.jsx';
+// import RelatedItems from './Components/RelatedItems/RelatedItems.jsx';
+// import Reviews from './Components/Reviews/Reviews.jsx';
 import axios from 'axios';
 const root = createRoot(document.getElementById("root"));
 
@@ -8,10 +16,10 @@ const root = createRoot(document.getElementById("root"));
 const App = () => {
   const [current, setCurrent] = useState({}); //current product
   const [rating, setRating] = useState(null);
-  console.log('rating:',rating)
+  //console.log('rating:',rating)
   function getProducts(id) {
     // console.log(`/products${id === undefined ? '' : '/' + id}`)
-    return axios.get(`/products${id === undefined ? '' : '/' + id}`, {
+    return axios.get(`/products${id === undefined ? '/37311' : '/' + id}`, {
       params: {
         product_id: id
       }
@@ -38,6 +46,7 @@ const App = () => {
       //   .then(res => setCurrent(res.data))
     }
   }, [])
+
   //console.log(current, 'the current')
   return ( <>
   <h1>Hello World</h1>
